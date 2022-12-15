cf11e11f3345f9464657abc74b5b35cad900ad6f
Rey-Of-Sunshine <msd.21@uni-dubna.ru>
2022-12-15 12:39:55 +0300

    hf

--- server.php
+++ server.php
@@ -1,2 +1,27 @@
-<?php
-echo "hello";
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "2074";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$data=file_get_contents("php://input");
+
+$sql = "INSERT INTO user (name, second_name, email, password, gender)
+VALUES ('" . $data['name_'] . "', '" . $data['second_name'] . "', '" . $data['email'] . "', '" . $data['passwd'] . "', '" . $data['gender'] . "')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
